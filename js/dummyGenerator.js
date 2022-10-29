27d1ac6682790703234ee14d07cf487cd26f6ea0
d-doshyn <d.doshyn@gmail.com>
2022-10-29 20:55:33 +0300

    problem with js syules

---
+++ js/dummyGenerator.js
@@ -0,0 +1,12 @@
+let dummyInputs = {
+    dummyWidth: document.querySelector(".width-dummy"),
+    dummyHeight: document.querySelector(".height-dummy"),
+    dummyBgColor: document.querySelector(".bgclr-dummy"),
+    dummyTextColor: document.querySelector(".textclr-dummy"),
+    dummyText: document.querySelector(".text-dummy"),
+    dummyFormat: document.querySelector(".format-dummy"),
+}
+
+let dummyImage = document.querySelector(".dummy-image").style.backgroundImage;
+
+console.log(dummyImage)
