69c2bfdb438733088f70223944900e43d6482f27
d-doshyn <d.doshyn@gmail.com>
2022-11-01 20:43:22 +0200

    add link copy alert

--- js/copyLink.js
+++ js/copyLink.js
@@ -5,6 +5,7 @@ const copyButtonPicsum = document.querySelector('.copyButton-picsum')
 copyButtonPicsum.addEventListener("click", function () {
     const picsumLinkContent = document.querySelector('.picsum-link').textContent
     navigator.clipboard.writeText(picsumLinkContent)
+    window.alert("Link copied!")
 })
 
 //DummyImage
@@ -14,4 +15,5 @@ const copyButtonDummy = document.querySelector('.copyButton-dummy')
 copyButtonDummy.addEventListener("click", function () {
     const dummyLInkContent = document.querySelector('.dummyimage-link').textContent
     navigator.clipboard.writeText(dummyLInkContent)
+    window.alert("Link copied!")
 })
