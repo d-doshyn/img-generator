e1cefd6e88edeec600ede9713db12b953daf389f
d-doshyn <d.doshyn@gmail.com>
2022-11-05 18:34:20 +0200

    fix nazars bugs and gave him informative comments

--- js/picsumGenerator.js
+++ js/picsumGenerator.js
@@ -10,26 +10,35 @@ let picsumInputs = {
 let image = document.querySelector('.lorem-picsum');
 let link = 'https://picsum.photos/';
 
-console.log(picsumInputs);
-console.log(image);
-
 // function
 function generateImg() {
-  let newImage = image.style.backgroundImage = 'url(' + link + picsumInputs.picsumWidth.value + '/' + picsumInputs.picsumHeight.value;
-  console.log(newImage);
+  let newImage = 'url(' + link + picsumInputs.picsumWidth.value + '/' + picsumInputs.picsumHeight.value + picsumInputs.picsumFormat.value;
 
+  // дужку додавай окремо, вона закінчує посилання, а після цього код не робить
+  image.style.backgroundImage = newImage + ")";
+  
   if (picsumInputs.picsumGrayscale.value == "Yes"){
-    let imageGrayscale = newImage + '?grayscale)'
-    newImage = imageGrayscale;
-    console.log(newImage);
-  }else if (picsumInputs.picsumBlur.value > 1) {
-    let imageBlur = newImage + `?blur=${picsumInputs.picsumBlur.value}`;
-    newImage = imageBlur;
-    console.log(newImage);
-  }else if (picsumInputs.picsumGrayscale.value == "yes" && picsumInputs.picsumBlur.value > 0){
-    let imageAll = newImage + '?grayscale&blur=' + picsumInputs.picsumBlur.value;
-    console.log(imageAll);
+    image.style.backgroundImage = newImage + '?grayscale' + ')'
+    // let imageGrayscale = newImage + '?grayscale)'
+    // newImage = imageGrayscale;
+    // console.log(imageGrayscale);
+  } else if (picsumInputs.picsumBlur.value > 0) {
+    image.style.backgroundImage = newImage + '?blur=' + picsumInputs.picsumBlur.value + ')'
+    // let imageBlur = newImage + `?blur=${picsumInputs.picsumBlur.value}`;
+    // newImage = imageBlur;
+    // console.log(imageBlur);
   }
+
+  // окремий іф, з елсом не працювало
+  if (picsumInputs.picsumGrayscale.value == "Yes" && picsumInputs.picsumBlur.value > 0){
+    image.style.backgroundImage = newImage + '?grayscale&blur=' + picsumInputs.picsumBlur.value + ')'
+    // let imageAll = newImage + '?grayscale&blur=' + picsumInputs.picsumBlur.value;
+    // console.log(imageAll);
+  }
+
+  // не знаю, нахєра тобі стільки змінних
+
+  console.log(image.style.backgroundImage);
 }
 
 for (let picsumInput in picsumInputs) {
