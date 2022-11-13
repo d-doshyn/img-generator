55170bb8367ae3945943b19329c27b8ff1ca5eff
Redsa123 <sergiiyoung@gmail.com>
2022-11-13 10:24:32 +0300

    added dark theme for like modal

--- js/black-theme.js
+++ js/black-theme.js
@@ -1,8 +1,9 @@
 let button = document.querySelector('.theme-button')
 let body = document.querySelector('body')
-let darkText = document.querySelectorAll('.theme-label, .title, .card-title, .footer__header, .icon-text, .footer-additionalText, .DummyImage-link, .LoremPicsum-link')
+let darkText = document.querySelectorAll('.theme-label, .title, .card-title, .footer__header, .icon-text, .footer-additionalText, .DummyImage-link, .LoremPicsum-link, .card-subtitle')
 let pictureIcon = document.querySelectorAll('i.fa-solid, i.fa-brands, i.fa-image, i.fa-square-h, i.fa-circle-dollar-to-slot, i.fa-instagram, i.fa-telegram, i.fa-comments')
-
+let likeBtn = document.querySelector('i.fa-thumbs-up')
+let likeContainer = document.querySelector('.open-donations')
 button.addEventListener("click", function () {
     body.classList.toggle('dark-background')
     darkText.forEach(elem => {
@@ -10,5 +11,12 @@ button.addEventListener("click", function () {
     })
     pictureIcon.forEach(elem => {
         elem.classList.toggle('dark-icon')
+    })
+    likeBtn.classList.toggle('dark-like-btn')
+    if(likeBtn.classList.contains('liked')) {
+        likeBtn.classList.toggle('liked-dark')
+    }
+    likeContainer.addEventListener("click", function() {
+        likeBtn.classList.toggle('liked-dark')
     })
 });
