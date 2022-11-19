6ca41d2b363c7f3c9ba28fc9290dd1c37ece1ca8
d-doshyn <d.doshyn@gmail.com>
2022-11-19 11:33:08 +0200

    clened our code and added fontawesome favicon

--- js/black-theme.js
+++ js/black-theme.js
@@ -4,18 +4,24 @@ let darkText = document.querySelectorAll('.theme-label, .title, .card-title, .fo
 let pictureIcon = document.querySelectorAll('i.fa-solid, i.fa-brands, i.fa-image, i.fa-square-h, i.fa-circle-dollar-to-slot, i.fa-instagram, i.fa-telegram, i.fa-comments')
 let likeBtn = document.querySelector('i.fa-thumbs-up')
 let likeContainer = document.querySelector('.open-donations')
+
 button.addEventListener("click", function () {
     body.classList.toggle('dark-background')
+
     darkText.forEach(elem => {
         elem.classList.toggle('dark-text')
     })
+
     pictureIcon.forEach(elem => {
         elem.classList.toggle('dark-icon')
     })
+
     likeBtn.classList.toggle('dark-like-btn')
+
     if(likeBtn.classList.contains('liked')) {
         likeBtn.classList.toggle('liked-dark')
     }
+    
     likeContainer.addEventListener("click", function() {
         likeBtn.classList.toggle('liked-dark')
     })
--- js/loremAttention.js
+++ js/loremAttention.js
@@ -3,6 +3,7 @@ let warningText = document.querySelector(".warning-text");
 
 warningBtn.addEventListener("click", () => {
     warningText.classList.toggle("block");
+    
     if (warningBtn.innerText == "Warning!") {
         warningBtn.innerText = "Close";
     } else {
